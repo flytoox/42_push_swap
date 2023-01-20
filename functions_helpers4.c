1d5f1061c034a2b09465873fa7ade5dc8c6f5e7e
flytoox <omarprince823@gmail.com>
2023-01-20 20:50:53 +0100

    i got this ;)

--- functions_helpers4.c
+++ functions_helpers4.c
@@ -6,7 +6,7 @@
 /*   By: obelaizi <obelaizi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 22:53:15 by obelaizi          #+#    #+#             */
-/*   Updated: 2023/01/18 23:02:04 by obelaizi         ###   ########.fr       */
+/*   Updated: 2023/01/20 20:49:31 by obelaizi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,10 @@
 void	fill_b_helper(t_stack **top_a, t_stack **top_b, int *cnt)
 {
 	pb(top_a, top_b);
-	if ((*top_a) && (*top_a)->index >= *cnt)
+	if (*top_a && (*top_a)->index >= *cnt)
 	{
-		if (ft_lstsize((*top_a)) - scan_last((*top_a), *cnt)
-			+ 1 > scan_first((*top_a), *cnt) - 1)
+		if (ft_lstsize(*top_a) - scan_last(*top_a, *cnt)
+			+ 1 >= scan_first(*top_a, *cnt) - 1)
 			rr(top_a, top_b);
 		else
 		{
@@ -34,13 +34,13 @@ void	fill_b(t_stack **top_a, t_stack **top_b, int chunk, int cnt)
 {
 	while ((*top_a)->index >= cnt)
 	{
-		if (ft_lstsize((*top_a)) - scan_last((*top_a), cnt)
-			+ 1 > scan_first((*top_a), cnt) - 1)
+		if (ft_lstsize(*top_a) - scan_last(*top_a, cnt)
+			+ 1 >= scan_first(*top_a, cnt) - 1)
 			ra(top_a);
 		else
 			rra(top_a);
 	}
-	if ((*top_a)->index >= cnt - (chunk / 2))
+	if ((*top_a)->index > cnt - (chunk / 2))
 		pb(top_a, top_b);
 	else
 		fill_b_helper(top_a, top_b, &cnt);
