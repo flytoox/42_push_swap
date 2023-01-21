9eca49cbc927d92b0514ef187934b7591518e3f7
flytoox <omarprince823@gmail.com>
2023-01-21 16:46:19 +0100

    need some Optimization in 500numbers

--- main_file.c
+++ main_file.c
@@ -36,21 +36,12 @@ int	give_me_that_chunck(int total)
 	else if (total <= 200)
 		chunk = total / 5;
 	else if (total <= 500)
-		chunk = total / 10;
+		chunk = total / 9;
 	else
 		chunk = total / 15;
 	return (chunk);
 }
 
-// void	print_lst(t_stack *head)
-// {
-// 	int i = 1;
-// 	while (head)
-// 	{
-// 		printf("%d |%d|", head->data, i++);
-// 		head = head->next;
-// 	}
-// }
 int	main(int argc, char *argv[])
 {
 	t_stack	*top_a;
